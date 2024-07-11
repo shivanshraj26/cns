0a647c2bd8aa589cc5df3b9c3e44a42071601044
Shivansh Raj <111772261+shivanshraj26@users.noreply.github.com>
2024-07-11 10:28:05 +0530

    Update p9.java

--- p9.java
+++ p9.java
@@ -6,15 +6,15 @@ public class sha {
         System.out.print("Enter the message to be hashed: ");
         String text = sc.nextLine();
 
-            MessageDigest digest = MessageDigest.getInstance("SHA-512");
-            byte[] hashBytes = digest.digest(text.getBytes("UTF-8"));
-            StringBuilder hexString = new StringBuilder();
+        MessageDigest digest = MessageDigest.getInstance("SHA-512");
+        byte[] hashBytes = digest.digest(text.getBytes("UTF-8"));
+        StringBuilder hexString = new StringBuilder();
             
-            for (byte hashByte : hashBytes) {
-                String hex = Integer.toHexString(0xff & hashByte);
-                if (hex.length() == 1) hexString.append('0');
-                hexString.append(hex);
-            }
-            System.out.println("SHA-512 Hash: " + hexString.toString());  
+        for (byte hashByte : hashBytes) {
+            String hex = Integer.toHexString(0xff & hashByte);
+            if (hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        System.out.println("SHA-512 Hash: " + hexString.toString());  
     }
 }
