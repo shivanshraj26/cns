c69b32cfcb3ab5edd6e705fa6abe8431d3dea09a
Shivansh Raj <111772261+shivanshraj26@users.noreply.github.com>
2024-07-11 11:27:46 +0530

    Update p10.java

--- p10.java
+++ p10.java
@@ -1,5 +1,6 @@
 import java.security.MessageDigest;
 import java.util.Scanner;
+
 public class md5 {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
