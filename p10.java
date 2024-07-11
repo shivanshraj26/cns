a6ded236c205e421819bd73e2cef61f2e08be137
Shivansh Raj <111772261+shivanshraj26@users.noreply.github.com>
2024-07-11 10:27:21 +0530

    Add files via upload

---
+++ p10.java
@@ -0,0 +1,20 @@
+import java.security.MessageDigest;
+import java.util.Scanner;
+public class md5 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the message to be hashed: ");
+        String text = sc.nextLine();
+
+        MessageDigest digest = MessageDigest.getInstance("MD5");
+        byte[] hashBytes = digest.digest(text.getBytes());
+        StringBuilder hexString = new StringBuilder();
+
+        for (byte hashByte : hashBytes) {
+            String hex = Integer.toHexString(0xff & hashByte);
+            if (hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        System.out.println("MD5 Hash: " + hexString.toString());  
+    }
+}
