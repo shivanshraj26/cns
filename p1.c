489e4ccfb6604715ebffafb116fd731fdaed74ed
Shivansh Raj <111772261+shivanshraj26@users.noreply.github.com>
2024-08-13 13:25:40 +0530

    Update p1.c

--- p1.c
+++ p1.c
@@ -8,7 +8,7 @@ int main()
     char str2[50];
     char str3[50];
     printf("Enter a string: ");
-    scanf("%[^\n]%*c", str);
+    scanf("%[^\n]", str);
     printf("The string is %s\n", str);
     int n = strlen(str);
     for (int i = 0; i < n; i++)
@@ -21,24 +21,33 @@ int main()
         str1[i] = str[i] & 127;
     }
     str1[n] = '\0';
-    printf("After doing AND --> %s", str1);
-    printf("\n");
+    printf("After doing AND --> %s\n", str1);
+    for (int i = 0; i < n; i++)
+    {
+        printf("ASCII for %c: %d\n", str[i], str1[i]);
+    }
 
     for (int i = 0; i < n; i++)
     {
         str2[i] = str[i] | 127;
     }
     str2[n] = '\0';
-    printf("After doing OR --> %s", str2);
-    printf("\n");
+    printf("After doing OR --> %s\n", str2);
+    for (int i = 0; i < n; i++)
+    {
+        printf("ASCII for %c: %d\n", str[i], str2[i]);
+    }
 
     for (int i = 0; i < n; i++)
     {
         str3[i] = str[i] ^ 127;
     }
     str3[n] = '\0';
-    printf("After doing XOR --> %s", str3);
-    printf("\n");
+    printf("After doing XOR --> %s\n", str3);
+    for (int i = 0; i < n; i++)
+    {
+        printf("ASCII for %c: %d\n", str[i], str3[i]);
+    }
 
     return 0;
 }
